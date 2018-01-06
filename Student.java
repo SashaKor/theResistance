43c10a12475e7d93b4535fd394433f35ca98f76d
akoroza <akoroza>
2018-01-05 21:52:05 -0500

    added Student.java + initial work

---
+++ Student.java
@@ -0,0 +1,95 @@
+/*
+superclass for all types of students
+
+similar to hw30-yorpg Character superclass
+ */
+
+public class Student{
+
+    // tracks social status in school as a whole
+    public int _socStat;
+    //tracks academic aptitude
+    public int _intelligence;
+    // mental health; functions as HP in that dropping below 0 = gameover
+    public int _mentHealth;
+    // physical health; functions as HP in that dropping below 0 = gameover
+    public int _physHealth;
+    // friend count;
+    public int friendCnt;
+    // tracking popularity amongst player's type
+    public int popularity;
+    // name; used for user experience
+    public String name;
+
+
+    //~~~~~~~~~~~~~~~~~~~~Mutators~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    public void changeSocStat(int change){
+	_socStat += change;
+    }
+
+    public void changeIntel(int change){
+	_intelligence += change;
+    }
+
+    public void changeMentHealth(int change){
+	_mentHealth += change;
+    }
+
+    public void changePhysHealth(int change){
+	_physHealth += change;
+    }
+
+    public void changeFriendCnt(int change){
+	_friendCnt += change;
+    }
+
+    public void changePopularity(int change){
+	_popularity += change;
+    }
+
+    public void setName(String newName){
+	_name = newName;
+    }
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    /*
+      printStats()-- prints stats for user
+      post: returns String of all attributes except name
+     */
+
+    public String printStats(){
+	String result= "";
+	result+= "Social Status: " + _socStat;
+	result+= "Intelligence: " + _intelligence;
+	result+= "Mental Health: " + _mentHealth;
+	result+= "Physical Health: " + _physHealth;
+	result+= "Friend Count: " + _friendCnt;
+	result+= "Popularity: " + _popularity;
+
+	return result;
+    }
+
+    /*
+      score()-- calculates score by taking into account all the attributes (other than name)
+      post: returns double result
+     */
+
+    public double score(){
+	//implementation
+    }
+
+    /*
+      randInt()-- returns a random integer range 1-5 (inclusive)
+     */
+    public int randInt(){
+	//implementation
+    }
+
+    /*
+      getDay()-- accessor to access day from driver file
+      (may not be necessary)
+     */
+}
